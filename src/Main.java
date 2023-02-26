ee6d002b4141ef5166e4a170f7256b3992864c55
annayrina <annayrina1998@mail.ru>
2023-02-26 14:05:48 +0700

    two task

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("task 1");
+        byte apple = -12;
+        short poteta = 350;
+        int banana = 4560000;
+        long gin = 500L;
+        float chocled = 1.2f;
+        double t = 3.54861;
+        double g = apple + poteta + banana + gin + chocled + t;
+        System.out.println(g);
+        System.out.println("task 2");
+        float c = 27.12f;
+        System.out.println(c);
+        long L = 987678965549L;
+        System.out.println(L);
+        double h = 2.786;
+        System.out.println(h);
+        int p = 569;
+        System.out.println(p);
+        int y = -159;
+        System.out.println(y);
+        int b = 27897;
+        System.out.println(b);
+        short s = 67;
+        System.out.println(s);
+        System.out.println("task 3");
+
+
+        }
+
+
+
+
+
+    }
